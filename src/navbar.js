3b7dad333c671912770f651a5171e02922a3c01a
THE_G <48765068+THEG77@users.noreply.github.com>
2022-04-21 00:35:34 +0530

    build

--- src/navbar.js
+++ src/navbar.js
@@ -3,12 +3,12 @@ import {Link, NavLink} from 'react-router-dom';
 import "./navbar.css"
 function Navbar() {
   return (
-      <div className='divNavbar'>
-        <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/" > Home </NavLink>
-        <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/custom" > Custom </NavLink>
-        <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/cart " > Cart </NavLink>
-        <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/dashboard" > Dashboard </NavLink>
-        <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/profile" > Profile </NavLink>
+      <div className='divUserNavbar'>
+        <NavLink exact className={(navData) => navData.isActive ? "active_usernavbar_class" : "unactive_usernavbar_class" } to="/" > Home </NavLink>
+        <NavLink exact className={(navData) => navData.isActive ? "active_usernavbar_class" : "unactive_usernavbar_class" } to="/custom" > Custom </NavLink>
+        <NavLink exact className={(navData) => navData.isActive ? "active_usernavbar_class" : "unactive_usernavbar_class" } to="/cart " > Cart </NavLink>
+        <NavLink exact className={(navData) => navData.isActive ? "active_usernavbar_class" : "unactive_usernavbar_class" } to="/dashboard" > Dashboard </NavLink>
+        <NavLink exact className={(navData) => navData.isActive ? "active_usernavbar_class" : "unactive_usernavbar_class" } to="/profile" > Profile </NavLink>
         {/* <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/Register" > Register </NavLink>
         <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/Login" > Login </NavLink> */}
         {/* <NavLink exact className={(navData) => navData.isActive ? "active_navbar_class" : "unactive_navbar_class" } to="/Welcome" > Welcome </NavLink> */}
